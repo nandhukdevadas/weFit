9a5f2b40ea20533e7e0ce956a8b958581eb9619e
Nandhu K Devadas <95268230+nandhukdevadas@users.noreply.github.com>
2024-09-11 22:20:56 +0530

    added analytics function from vercel

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,23 +4,26 @@ import MuscleGroupSwiper from '../Components/MuscleGroupSwiper';
 import MuscleMovement from '../Components/MuscleMovement';
 import Weekly from '../Components/Weekly';
 import { motion } from 'framer-motion';
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 const Home = () => {
   
   return (
-    <motion.div 
+    <motion.div
     cstyle={{ backgroundColor: 'black' }}
-    initial={{x: "100%"}}
+    initial={{x: "50%"}}
     animate={{x: "0%"}}
     transition={{duration: 0.3, ease: "easeOut"}}
     exit={{opacity: 1}}
+    className='home-body'
     >
         <Navbar/>
         <MuscleGroupSwiper/>
         <MuscleMovement/>
         <Weekly/>
         <SpeedInsights/>
+        <Analytics/>
     </motion.div>
   )
 }
